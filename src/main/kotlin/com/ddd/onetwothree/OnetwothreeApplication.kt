d523c3b91640abc512675ee2d2d5da209fee3208
slolee <slolee@naver.com>
2022-11-30 01:20:50 +0900

    Project Initialize

---
+++ src/main/kotlin/com/ddd/onetwothree/OnetwothreeApplication.kt
@@ -0,0 +1,11 @@
+package com.ddd.onetwothree
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class OnetwothreeApplication
+
+fun main(args: Array<String>) {
+    runApplication<OnetwothreeApplication>(*args)
+}
